f5ea6c38af35f7cc8339f2559e199a28a8c405cd
hassanpc35 <84283300+hassanpc35@users.noreply.github.com>
2021-08-09 22:48:41 +0100

    Delete lib directory

--- lib/route/route_constant.dart
+++
@@ -1,9 +0,0 @@
-const String splashRoute = '/splash';
-const String registrationRoute = '/registration';
-const String loginRoute = '/login';
-const String homeRoute = '/home';
-const String addRoom = '/addRoom';
-const String forgetPasswordRoute = '/forgotPassword';
-const String addAppliancesRoute = '/addAppliances';
-const String analyticsRoute ='/analytics';
-const String dashboardRoute ='/dashboard';
